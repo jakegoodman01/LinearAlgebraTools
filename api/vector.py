b50e7f01e5aa75c2f527db8f0870aaf32dfc4573
Jake Goodman <jakegoodman@Jakes-iMac.local>
2021-04-02 11:17:33 -0400

    added api and tests directories

---
+++ api/vector.py
@@ -0,0 +1,79 @@
+
+from typing import List
+
+"""------------------------VECTOR STUFF------------------------"""
+
+
+class Vector:
+    def __init__(self, nums: List[int]):
+        self.dim = len(nums)
+        assert self.dim > 0, "Empty Coordinates List"
+        self.components = nums
+
+    def __repr__(self):
+        output = f'(\n {self.components[0]},\n'
+        for i in range(1, self.dim):
+            output += f' {self.components[i]},\n'
+        output += ')'
+        return output
+
+    # zero_vector() produces the zero_vector in the same dimension as self
+    # time: O(1)
+    def zero_vector(self):
+        zero = Vector([0] * self.dim)
+        return zero
+
+
+# vector_equals(v, w) produces true if v equals w
+# time: O(n)
+def vector_equals(v: Vector, w: Vector) -> bool:
+    if v.dim == w.dim:
+        for i in range(v.dim):
+            if v.components[i] != w.components[i]:
+                return False
+        return True
+    return False
+
+
+# vector_add(v, w, *args) produces the sum of v, w and args
+# time: O(n)
+def vector_add(v: Vector, w: Vector, *args: Vector) -> Vector:
+    assert v.dim == w.dim, "Can't add vectors of different dimensions"
+    new_vector = v.zero_vector()
+    for i in range(v.dim):
+        new_vector.components[i] = v.components[i] + w.components[i]
+    for vector in args:
+        new_vector = vector_add(new_vector, vector)
+    return new_vector
+
+
+# vector_negate(v) produces -v
+# time: O(n)
+def vector_negate(v: Vector) -> Vector:
+    new_vector = v.zero_vector()
+    for i in range(v.dim):
+        new_vector.components[i] -= v.components[i]
+    return new_vector
+
+
+# vector_subtract(v, w) produces the difference v - w
+# time: O(n)
+def vector_subtract(v: Vector, w: Vector) -> Vector:
+    return vector_add(v, vector_negate(w))
+
+
+# vector_scalar_multiply(v, s) produces the product of v and s
+# time: O(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tests/test_vector.py
@@ -0,0 +1,102 @@
+import unittest
+
+from api import vector as vc
+
+
+class TestVector(unittest.TestCase):
+    def setUp(self):
+        # Create some vectors
+        self.v1 = vc.Vector([1, 2, 3])
+        self.v2 = vc.Vector([1, 2, 3])
+        self.v3 = vc.Vector([1, 2, 4])
+        self.u = vc.Vector([3, -5, 7])
+        self.w = vc.Vector([9, 2, -1])
+        self.zero3 = self.v1.zero_vector()
+        self.v1plusv3 = vc.vector_add(self.v1, self.v3)
+
+    def test_constructor(self):
+        self.assertEqual(self.v1.dim, 3)
+        self.assertEqual(self.v1.components[0], 1)
+        self.assertEqual(self.v1.components[1], 2)
+        self.assertEqual(self.v1.components[2], 3)
+
+    def test_zero_vector(self):
+        self.assertEqual(self.zero3.dim, 3)
+        for i in range(3):
+            self.assertEqual(self.zero3.components[i], 0)
+
+    def test_vector_equals(self):
+        self.assertTrue(vc.vector_equals(self.v1, self.v2))
+        self.assertFalse(vc.vector_equals(self.v1, self.v3))
+
+    def test_vector_add(self):
+        self.assertTrue(
+            vc.vector_equals(self.v1plusv3, vc.Vector([2, 4, 7]))
+        )
+        self.assertTrue(
+            # Asserts: u + 0 = 0 + u = u
+            vc.vector_equals(
+                vc.vector_add(self.zero3, self.v1),
+                vc.vector_add(self.v1, self.zero3)
+            )
+            and
+            vc.vector_equals(
+                vc.vector_add(self.v1, self.zero3),
+                self.v1
+            )
+        )
+        self.assertTrue(
+            # Asserts: v + u + w = v + (u + w)
+            vc.vector_equals(
+                vc.vector_add(self.v1, self.u, self.w),
+                vc.vector_add(self.v1, vc.vector_add(self.u, self.w))
+            )
+        )
+        self.assertTrue(
+            # Asserts: v + u + w = (v + u) + w
+            vc.vector_equals(
+                vc.vector_add(self.v1, self.u, self.w),
+                vc.vector_add(vc.vector_add(self.v1, self.u), self.w)
+            )
+        )
+        self.assertTrue(
+            # Asserts: v + u = u + v
+            vc.vector_equals(
+                vc.vector_add(self.v1, self.u), vc.vector_add(self.u, self.v1)
+            )
+        )
+
+    def test_vector_negate(self):
+        self.assertTrue(
+            vc.vector_equals(
+                vc.vector_negate(self.w),
+                vc.Vector([-9, -2, 1])
+            )
+        )
+        self.assertTrue(
+            vc.vector_equals(self.zero3, vc.vector_negate(self.zero3))
+        )
+        self.assertTrue(
+            vc.vector_equals(
+                vc.vector_add(vc.vector_negate(self.w), self.w),
+                self.zero3
+            )
+        )
+
+    def test_vector_subtract(self):
+        self.assertTrue(
+            vc.vector_equals(
+                vc.vector_subtract(self.v1, self.u),
+                vc.Vector([-2, 7, -4])
+            )
+        )
+        self.assertTrue(
+            vc.vector_equals(
+                vc.vector_subtract(self.w, self.w),
+                self.zero3
+            )
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
